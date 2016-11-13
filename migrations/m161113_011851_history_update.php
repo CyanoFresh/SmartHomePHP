11995c81b555b01bed244455d9618666048cc41a
Alex Solomaha <cyanofresh@gmail.com>
2016-11-13 17:19:46 +0200

    Fixes in migration

--- migrations/m161113_011851_history_update.php
+++ migrations/m161113_011851_history_update.php
@@ -9,7 +9,7 @@ class m161113_011851_history_update extends Migration
         $this->addColumn('history', 'type', $this->smallInteger()->after('id')->notNull()->defaultValue(10));
         $this->addColumn('history', 'event_id', $this->integer()->after('type'));
         $this->addColumn('history', 'board_id', $this->integer()->after('event_id'));
-        $this->addColumn('history', 'user_id', $this->integer()->after('board_id'));
+        $this->alterColumn('history', 'user_id', $this->integer());
     }
 
     public function safeDown()
@@ -17,6 +17,6 @@ class m161113_011851_history_update extends Migration
         $this->dropColumn('history', 'type');
         $this->dropColumn('history', 'event_id');
         $this->dropColumn('history', 'board_id');
-        $this->dropColumn('history', 'user_id');
+        $this->alterColumn('history', 'user_id', $this->integer()->notNull());
     }
 }
