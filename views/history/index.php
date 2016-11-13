e3c5f7db9928018ff2385bb80981473ff7135c8c
Alex Solomaha <cyanofresh@gmail.com>
2016-11-13 17:54:30 +0200

    Fixes view

--- views/history/index.php
+++ views/history/index.php
@@ -4,25 +4,23 @@
 /* @var $searchModel app\models\HistorySearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-use yii\helpers\Html;
 use yii\widgets\ListView;
 use yii\widgets\Pjax;
-$this->title = 'Histories';
+
+$this->title = 'История';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="history-index">
 
-<!--    <p>-->
-<!--        --><?//= Html::a('Добавить', ['create'], ['class' => 'btn btn-success']) ?>
-<!--    </p>-->
-<!--    --><?php //echo $this->render('_search', ['model' => $searchModel]); ?>
-
     <?php Pjax::begin(); ?>
+
     <?= ListView::widget([
         'dataProvider' => $dataProvider,
         'itemOptions' => ['class' => 'item'],
         'summaryOptions' => ['class' => 'alert alert-info'],
+        'emptyTextOptions' => ['class' => 'alert alert-warning'],
         'itemView' => '_history',
     ]) ?>
+
     <?php Pjax::end(); ?>
 </div>
