856a89388e28603597e2218c6d096cb58eb680e3
CyanoFresh <cyanofresh@gmail.com>
2017-08-03 22:35:55 +0300

    Large chart modal

--- views/panel/index.php
+++ views/panel/index.php
@@ -164,7 +164,7 @@ $this->title = 'Панель Управления';
 
 <!-- Item Chart Modal -->
 <div class="modal fade" id="item-chart-modal" tabindex="-1" role="dialog" aria-labelledby="item-chart-modal-label">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
